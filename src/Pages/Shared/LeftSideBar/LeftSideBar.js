37f820f1b1ef76755577d92ce897f77c241b4cb4
Mazharul Islam <mazharul430@gmail.com>
2022-10-22 10:19:17 -0700

    add folder structure

---
+++ src/Pages/Shared/LeftSideBar/LeftSideBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftSideBar = () => {
+    return (
+        <div>
+            <h1>This is Left Side Page</h1>
+        </div>
+    );
+};
+
+export default LeftSideBar;
---
+++ src/Pages/Shared/RightSideBar/RightSideBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideBar = () => {
+    return (
+        <div>
+            <h1>This is Right Bar</h1>
+        </div>
+    );
+};
+
+export default RightSideBar;
